5fe2e03bd811bb2dfbd35c034f3246e3b8499032
Renan Teixeira <renan.r.teixeira@gmail.com>
2026-05-04 20:26:47 -0300

    startup file adjustment

--- webapp/src/main/java/com/controle/ponto/webapp/PontoApplication.java
+++ webapp/src/main/java/com/controle/ponto/webapp/PontoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,8 +16,24 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EntityScan(basePackages = "com.controle.ponto.domain")
 public class PontoApplication extends SpringBootServletInitializer {
 
+	private static final String LOGBACK = "/ponto/logback.xml";
+
 	public static void main(String[] args) {
 		SpringApplication.run(PontoApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+		String path = System.getProperty("app.configFolder");
+
+		if (path != null) {
+			System.setProperty(
+					"logging.config",
+					path + LOGBACK
+			);
+		}
+
+		return application.sources(PontoApplication.class);
+	}
 }
